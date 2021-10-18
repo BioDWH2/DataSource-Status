ad929cbe6721139bfb899b898a5af99e1ea18c49
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2021-10-18 16:32:48 +0200

    Finish DrugCentral

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -1,10 +1,16 @@
+import re
 import json
+from urllib import request
 
 
 def get_drugcentral_entry():
+    source = request.urlopen('https://drugcentral.org/ActiveDownload').read().decode('utf-8')
+    pattern = re.compile(
+        '(https://unmtid-shinyapps\\.net/download/drugcentral\\.dump\\.([0-9]+)_([0-9]+)_([0-9]{4})\\.sql\\.gz)')
+    matches = pattern.findall(source)
     return {
-        'version': None,
-        'files': []
+        'version': matches[0][1] + '.' + matches[0][2] + '.' + matches[0][3] if len(matches) > 0 else None,
+        'files': [matches[0][0]] if len(matches) > 0 else []
     }
 
 
